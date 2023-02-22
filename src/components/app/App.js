0719a469f95d81274d0b44300b41fc8c8ba6b091
Vladyslav Rozalyuk <vladyslav.rozalyuk@gmail.com>
2023-02-22 18:09:41 +0100

    created Header component

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,3 +1,4 @@
+import Header from '../header/Header'
 import Content from '../content/Content'
 import Collection from '../collection/Collection'
 import CollectionItemInfo from '../collectionItemInfo/CollectionItemInfo'
@@ -7,6 +8,7 @@ import './App.css'
 function App() {
   return (
     <div className="App">
+      <Header />
       <Content
         collection={<Collection />}
         collectionItemInfo={<CollectionItemInfo />}
---
+++ src/components/header/Header.js
@@ -0,0 +1,13 @@
+const Header = () => {
+  return (
+    <header>
+      <h1 className="text-start text-uppercase fs-1 fw-bold lh-lg">
+        <a href="#">
+          <span>The Metropolitan Museum of Art</span> information portal
+        </a>
+      </h1>
+    </header>
+  )
+}
+
+export default Header
