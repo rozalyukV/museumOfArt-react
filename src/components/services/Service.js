70a416bd489b600fa0f2140bfcb4d4ac3c64f251
Vladyslav Rozalyuk <vladyslav.rozalyuk@gmail.com>
2023-02-23 17:30:29 +0100

    added server request function 'getItem' in Service component

--- src/components/services/Service.js
+++ src/components/services/Service.js
@@ -1,18 +1,21 @@
 class Services {
+  _apiBase = 'https://collectionapi.metmuseum.org/public/collection/v1/'
+
   getResource = async (url) => {
     let res = await fetch(url)
 
     if (!res.ok) {
       throw new Error(`Could not fetch ${url}, status: ${res.status}`)
     }
-
     return await res.json()
   }
 
   getAllItems = () => {
-    return this.getResource(
-      'https://collectionapi.metmuseum.org/public/collection/v1/objects'
-    )
+    return this.getResource(`${this._apiBase}objects`)
+  }
+
+  getItem = (id) => {
+    return this.getResource(`${this._apiBase}objects/${id}`)
   }
 }
 
