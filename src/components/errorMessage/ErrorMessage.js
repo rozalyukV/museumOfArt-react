bdf792a897440c1ec855a993006fcdc2d2160c9a
Vladyslav Rozalyuk <vladyslav.rozalyuk@gmail.com>
2023-02-25 21:41:48 +0100

    created ErrorMessage component

---
+++ src/components/errorMessage/ErrorMessage.js
@@ -0,0 +1,18 @@
+import img from './error.gif'
+
+const ErrorMessage = () => {
+  return (
+    <img
+      styles={{
+        display: 'block',
+        width: '250px',
+        height: '250px',
+        objectFit: 'contain',
+        margin: '0 auto',
+      }}
+      src={img}
+    />
+  )
+}
+
+export default ErrorMessage
