d85d34a8035e2f6bc9176093223926335b706e47
Vladyslav Rozalyuk <vladyslav.rozalyuk@gmail.com>
2023-03-03 09:53:31 +0100

    modificated Header component

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -3,7 +3,9 @@ const Header = () => {
     <header className="container px-4 my-4">
       <h1 className="text-start text-uppercase fs-2 fw-bold lh-lg">
         <a href="#">
-          <span className="text-danger">The Metropolitan Museum of Art</span>{' '}
+          <span style={{ color: '#20c997' }}>
+            The Metropolitan Museum of Art
+          </span>{' '}
           information portal
         </a>
       </h1>
