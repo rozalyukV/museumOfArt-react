a50bc3ddcba7c5133aa18a46087c75c5824212ee
Vladyslav Rozalyuk <vladyslav.rozalyuk@gmail.com>
2023-03-03 09:53:10 +0100

    modificated Content component

--- src/components/content/Content.js
+++ src/components/content/Content.js
@@ -1,12 +1,12 @@
 const Content = (props) => {
   return (
     <div className="container px-4 text-center">
-      <div className="row gx-5">
-        <div className="col-8">
-          <div className="p-3 border bg-light">{props.collection}</div>
+      <div className="row g-4">
+        <div className="col-12 col-lg-8 order-last order-lg-first">
+          <div>{props.collection}</div>
         </div>
-        <div className="col-4">
-          <div className="p-3 border bg-light">{props.collectionItemInfo}</div>
+        <div className="col col-lg-4">
+          <div>{props.collectionItemInfo}</div>
         </div>
       </div>
     </div>
