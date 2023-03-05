a72b28fbd1a324d147ec5d70a5353ad87bcc0eca
Vladyslav Rozalyuk <vladyslav.rozalyuk@gmail.com>
2023-03-05 14:06:44 +0100

    modificated App component

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,20 +1,45 @@
+import { Component } from 'react'
+
 import Header from '../header/Header'
 import Content from '../content/Content'
 import Collection from '../collection/Collection'
 import CollectionItemInfo from '../collectionItemInfo/CollectionItemInfo'
 import RandomItem from '../randomItem/RandomItem'
+import ErrorBoundary from '../errorBoundary/ErrorBoundary'
 
-function App() {
-  return (
-    <div className="App">
-      <Header />
-      <RandomItem />
-      <Content
-        collection={<Collection />}
-        collectionItemInfo={<CollectionItemInfo />}
-      />
-    </div>
-  )
+class App extends Component {
+  state = {
+    selectedItem: null,
+  }
+
+  onItemSelected = (id) => {
+    this.setState({
+      selectedItem: id,
+    })
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <Header />
+        <ErrorBoundary>
+          <RandomItem />
+        </ErrorBoundary>
+        <Content
+          collection={
+            <ErrorBoundary>
+              <Collection onItemSelected={this.onItemSelected} />
+            </ErrorBoundary>
+          }
+          collectionItemInfo={
+            <ErrorBoundary>
+              <CollectionItemInfo itemId={this.state.selectedItem} />
+            </ErrorBoundary>
+          }
+        />
+      </div>
+    )
+  }
 }
 
 export default App
